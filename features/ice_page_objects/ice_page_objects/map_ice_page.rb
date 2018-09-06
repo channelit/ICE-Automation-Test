7f7fe4b8fa4b4fe1d7b80a757b1a5cce4c2cb959
Montana <info@theallianceone.com>
2018-09-05 20:10:04 -0400

    adding run config for ice

--- features/ice_page_objects/ice_page_objects/map_ice_page.rb
+++ features/ice_page_objects/ice_page_objects/map_ice_page.rb
@@ -4,7 +4,7 @@ def map_ice_page
 
     {
         #Export librarian, adding a country.
-        'sample 1'                                   =>"xpath^^.//*[@id='ctl00_ctl00_UserRoleDropDownList']",
+        'sample 1'                                   =>"xpath^^.//*[@id='menu-item-143']/a",
         'sample 2'                                   =>"xpath^^.//*[@id='ctl00_ctl00_ContentPlaceHolder1_AppMenuBar']/ul/li[1]/a/span/span[1]",
 
     }

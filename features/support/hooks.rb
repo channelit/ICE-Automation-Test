a6321aee1d0c0f440dbecb113ef1c4cf578b5011
MontanaDeric <43016256+MontanaDeric@users.noreply.github.com>
2018-09-06 18:22:27 -0400

    adding draft test for admin login (#3)

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,18 +6,28 @@ require 'selenium-webdriver'
 
 EXEC_ID = Time.now.strftime('%m%d%Y%H%M%S')
 RESULTS_DIR = "#{Dir.pwd}/features/results/#{EXEC_ID}"
+if ENV['TEST_ENV'].downcase.eql? 'test08'
+  TIMEOUT = 40
+  puts 'this is the wait(40s) we are using for Prod ENV'
+else
+  TIMEOUT = 45
+  if ENV['TEST_ENV'].downcase.eql? 'test06'
+    TIMEOUT = 45
+    puts 'this is the wait(45s) we are using for Test ENV'
+  else
+    TIMEOUT = 45
+    puts 'this is the wait we are using for none test nor prod ENV'
+  end
+end
+
 
 SCREEN_SHOT = false
 
-if ENV['TEST_ENV'].downcase.include? 'score'
-  TIMEOUT = 40
-  @@IMPLICIT_WAIT = 30
-else
-  TIMEOUT = 60
-  @@IMPLICIT_WAIT = 60
-end
 # puts "Executing on browser #{Configuration.browser.downcase}"
 BROWSER_NAME = Configuration.browser.downcase
+
+
+
 
 #********************To open Browser at one time and run all scenario*********************
 
@@ -30,38 +40,64 @@ selenium_driver.open_timeout = TIMEOUT
 
 @@browser = Watir::Browser.new BROWSER_NAME.to_sym, :http_client => selenium_driver
 
-@@browser.driver.manage.timeouts.implicit_wait= @@IMPLICIT_WAIT
+@@browser.driver.manage.timeouts.implicit_wait=35
 # @browser = get_browser
-@@browser.driver.manage.window.maximize
+#@@browser.driver.manage.window.maximize
 
 Before do
   @browser = @@browser
 end
 
 
-# # == Open browser on each scenario and then close browser
-# def get_browser
-#   http_driver = Selenium::WebDriver::Remote::Http::Default.new
-#   http_driver.read_timeout = TIMEOUT
-#   http_driver.open_timeout = TIMEOUT
-#   # client.timeout = TIMEOUT # seconds – default is 30
-#   @browser = Watir::Browser.new BROWSER_NAME.to_sym, :http_client => http_driver
-#   # @browser.driver.manage.window.maximize
-#   @browser.driver.manage.timeouts.implicit_wait=30
-#   # screen_width = @browser.execute_script("return screen.width;")
-#   # screen_height = @browser.execute_script("return screen.height;")
-#   # @browser.driver.manage.window.resize_to(screen_width,screen_height)
-#   @browser
-# end
-#
-# Before do
-#   @browser = get_browser
-#   @browser.driver.manage.window.maximize
-# # end
-#
-# After do
-#   @browser.close
-# end
+# == Open browser on each scenario and then close browser
+def get_browser
+  http_driver = Selenium::WebDriver::Remote::Http::Default.new
+  http_driver.read_timeout = TIMEOUT
+  http_driver.open_timeout = TIMEOUT
+  # client.timeout = TIMEOUT # seconds – default is 30
+  @browser = Watir::Browser.new BROWSER_NAME.to_sym, :http_client => http_driver
+  # @browser.driver.manage.window.maximize
+  @browser.driver.manage.timeouts.implicit_wait=30
+  # screen_width = @browser.execute_script("return screen.width;")
+  # screen_height = @browser.execute_script("return screen.height;")
+  # @browser.driver.manage.window.resize_to(screen_width,screen_height)
+  @browser
+
+end
+
+Before do
+  if RUBY_PLATFORM.downcase.include?("linux")
+    @browser.driver.manage.timeouts.implicit_wait=30
+    @browser.driver.manage.window.resize_to(1600, 900)
+    @browser.goto "http://allianceone.global/"
+    #@browser.driver.manage.window.maximize
+    @browser.driver.manage.timeouts.implicit_wait=15
+    puts("inspect: #{@browser.inspect}")
+    puts("status : #{@browser.status}")
+    puts("text   : #{@browser.text}")
+    puts("html   : #{@browser.html}")
+    @browser = get_browser
+    @browser.window.resize_to(1366, 768)
+    puts @browser.html
+    puts @browser
+    puts 'The OS Platform is ' +RUBY_PLATFORM
+  else
+    @browser = get_browser
+    @browser.window.resize_to(1366, 768)
+    @browser.driver.manage.window.maximize
+    puts("inspect: #{@browser.inspect}")
+    puts("status : #{@browser.status}")
+    puts("text   : #{@browser.text}")
+    puts("html   : #{@browser.html}")
+    puts @browser.html
+    puts @browser.browser
+    @browser.driver.manage.timeouts.implicit_wait=15
+    puts 'The OS platform is ' +RUBY_PLATFORM
+  end
+end
+After do
+  @browser.close
+end
 
 
 # Before ('@mobile') do
@@ -82,21 +118,21 @@ end
 #   end
 # end
 
-Before do |scenario|
-  # puts "Executing Test on #{Configuration.host}"
-  SCENARIO_ID = (scenario.name).delete('^0-9')
-  if SCENARIO_ID.empty?
-    SCENARIO_ID = scenario.name[0, 15]
-  end
-  SCREENSHOT_NAME = "/#{scenario.__id__}"
-end
-
-After do |scenario|
-  if scenario.failed?
-    clear_alert
-    capture_screenshot
-  end
-end
+# Before do |scenario|
+#   # puts "Executing Test on #{Configuration.host}"
+#   SCENARIO_ID = (scenario.name).delete('^0-9')
+#   if SCENARIO_ID.empty?
+#     SCENARIO_ID = scenario.name[0, 15]
+#   end
+#   SCREENSHOT_NAME = "/#{scenario.__id__}"
+# end
+#
+# After do |scenario|
+#   if scenario.failed?
+#     clear_alert
+#     capture_screenshot
+#   end
+# end
 
 def clear_alert
   if @browser.alert.exists?
